5ae6ea831ec6a0acf38bec379350e8b0a82f581b
BMRaed7 <bmraed7@gmail.com>
2021-09-02 17:47:23 +0100

    make it better4

--- src/upload.middleware.js
+++ src/upload.middleware.js
@@ -17,7 +17,7 @@ const months = { 0: 'January', 1: 'February', 2: 'March', 3: 'April', 4: 'May', 
 
 var storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, __basedir + "/public/uploads/");
+    cb(null, "/public/uploads/");
   },
   filename: (req, file, cb) => {
     let Now = new Date();
